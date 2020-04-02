5d5a17968a155403ac4cda3308282e8d4bb34f87
Sia Karamalegos <siakaramalegos@gmail.com>
2020-04-02 13:09:50 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// const username = document.getElementById('username')
+// console.log(username)
+const greeting = "Hello"
+let myName = "Sia"
+
+// console.log(greeting + ' ' + myName);
+
+myName = "George"
+
+// console.log(greeting + ' ' + myName);
